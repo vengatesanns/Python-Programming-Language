cbe58d2af2a43b0695d644238b244d4d9899ea54
Vengatesan Nagarajan <vengat19amd@gmail.com>
2021-09-08 11:50:12 +0530

    Data Types

---
+++ basics/Datatypes.py
@@ -0,0 +1,13 @@
+
+
+# int, str, float, bool, list, tuple, set, dict
+
+print(1 + 1) # => 2
+print(1 - 1) # => 0
+print(5 / 2) # => 2.5
+print(10 * 2) # => 20
+print(2**3)  # => 8 (Power)
+print(2//3)  # => 0 (Round Off)
+print(3//2)  # => 1 
+print(3%2)   # => 1
+print(27%3)  # => 0
